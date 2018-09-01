74360ae471e80951ed6690ca7db84daa6dd62862
Patricio Goncevatt <patriciogoncevatt@gmail.com>
2018-09-01 18:45:22 -0300

    make ProjectRecord class make TaskRecord class make ProjectReader class

--- packages/store_tools.py
+++ packages/store_tools.py
@@ -1,6 +1,3 @@
-from packages.date_tools import Project
-from packages.date_tools import Date
-from packages.date_tools import Task
 import os
 
 
@@ -27,3 +24,58 @@ class ProjectWriter(object):
 
     def close(self):
         self.file_obj.close()
+
+    def get_path(self):
+        return self.path
+
+
+class ProjectRecord(object):
+    def __init__(self):
+        self.name = None
+        self.description = None
+        self.from_date = None
+        self.to_date = None
+        self.tasks = None
+
+
+class TaskRecord(object):
+    def __init__(self):
+        self.name = None
+        self.notes = None
+        self.date = None
+
+
+class ProjectReader(object):
+    def __init__(self, path):
+        assert os.path.isfile(path), "No file"
+        self.file_path = path
+        self.file_ob = None
+
+    def _read_task(self):
+        my_tasks = list()
+        my_line = self.file_ob.readline().strip()
+        while True:
+            if my_line == ".":
+                task_ins = TaskRecord()
+                task_ins.name = self.file_ob.readline().strip()
+                task_ins.date = self.file_ob.readline().strip()
+                task_ins.notes = self.file_ob.readline().strip()
+                my_tasks.append(task_ins)
+                my_line = self.file_ob.readline().strip()
+            else:
+                break
+        return my_tasks
+
+    def open(self):
+        self.file_ob = open(self.file_path, 'r')
+
+    def read(self):
+        out_pro = ProjectRecord()
+        out_pro.name = self.file_ob.readline().strip()
+        out_pro.description = self.file_ob.readline().strip()
+        out_pro.from_date = self.file_ob.readline().strip()
+        out_pro.to_date = self.file_ob.readline().strip()
+        out_pro.tasks = self._read_task()
+
+    def close(self):
+        self.file_ob.close()
